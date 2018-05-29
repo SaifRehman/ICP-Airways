708b8b3dc0d3d245664fdc0ee54de9543b82bf30
saifrehman <s4saif.121@gmail.com>
2018-05-29 11:40:29 +0400

    added history home login sigup page

--- icp-frontend/src/app/app.module.ts
+++ icp-frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
