214d471eb1561f153df87bba065a0fec4f8a4458
saifrehman <s4saif.121@gmail.com>
2018-05-29 11:48:41 +0400

    #2 added routing to front end

---
+++ icp-frontend/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+import { HistoryComponent } from './history/history.component'
+import { LoginComponent } from './login/login.component'
+import { SignupComponent } from './signup/signup.component'
+import { HomeComponent } from './home/home.component'
+ 
+export const AppRoutes: Routes = [
+    { 
+        path: 'history', component: HistoryComponent,
+    },
+    { 
+        path: 'home', component: HomeComponent,
+    },
+    { 
+        path: 'login', component: LoginComponent,
+    },
+    { 
+        path: 'signup', component: SignupComponent,
+    }
+];
+ 
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
