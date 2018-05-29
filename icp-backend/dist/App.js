2ccf4f7dd967deebf1bb6359abf1cd1aff2bda5e
saifrehman <s4saif.121@gmail.com>
2018-05-29 11:08:55 +0400

    backend stable with db2 installed

--- icp-backend/dist/App.js
+++ icp-backend/dist/App.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-var ibmdb = require('ibm_db');
 class App {
     constructor() {
         this.express = express();
@@ -17,22 +16,19 @@ class App {
     routes() {
         let router = express.Router();
         router.get('/test', (req, res, next) => {
-            ibmdb.open("DATABASE=SAMPLE;HOSTNAME=localhost;UID=db2inst1;PWD=db2inst1-pwd;PORT=50000;PROTOCOL=TCPIP", function (err, conn) {
-                if (err)
-                    return console.log(err);
-                conn.query('select * from DB2INST1.VSTAFAC2', function (err, data) {
-                    if (err)
-                        console.log(err);
-                    else
-                        console.log(data);
-                    res.json({
-                        message: data
-                    });
-                    conn.close(function () {
-                        console.log('done');
-                    });
-                });
-            });
+            // ibmdb.open("DATABASE=SAMPLE;HOSTNAME=localhost;UID=db2inst1;PWD=db2inst1-pwd;PORT=50000;PROTOCOL=TCPIP", function (err, conn) {
+            //   if (err) return console.log(err);
+            //   conn.query('select * from DB2INST1.VSTAFAC2', function (err, data) {
+            //     if (err) console.log(err);
+            //     else console.log(data);
+            //     res.json({
+            //       message: data
+            //     });
+            //     conn.close(function () {
+            //       console.log('done');
+            //     });
+            //   });
+            // });
         });
         this.express.use('/', router);
     }
