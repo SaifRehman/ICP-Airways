2532f0b7b2bde5eb5c68aede8a22d29ea2c52085
saifrehman <s4saif.121@gmail.com>
2018-05-29 13:17:19 +0400

    changed css to scss

--- icp-frontend/src/app/history/history.component.ts
+++ icp-frontend/src/app/history/history.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
 
--- icp-frontend/src/app/signup/signup.component.ts
+++ icp-frontend/src/app/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
