23d57c2364d2dbf3ef85db21c79f0983a95cbdd0
saifrehman <s4saif.121@gmail.com>
2018-05-30 15:36:23 +0400

    #4 env var fixed

--- icp-backend/signup-microservice/src/App.ts
+++ icp-backend/signup-microservice/src/App.ts
@@ -2,20 +2,17 @@ import * as path from 'path';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv/config'
 var ibmdb = require('ibm_db');
 
 class App {
   public express: express.Application;
-  public connectionString = "";
-  public dotenv :any
+  public connectionString ;
   constructor() {
-    dotenv.config();
     this.connectionString = 'DATABASE='+process.env.DATABASE+';'+
     'HOSTNAME='+ process.env.HOSTNAME+';'+'UID='+process.env.UID+';'+
     'PWD='+process.env.PASSWORD+';'+'PORT='+process.env.PORT+';'+
     'PROTOCOL='+process.env.PROTOCOL+';'
-    console.log(this.connectionString);
     this.express = express();
     this.middleware();
     this.routes();
