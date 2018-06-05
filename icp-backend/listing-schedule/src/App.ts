832b3259922a90b44a872bd9763112ff11260d28
saifrehman <s4saif.121@gmail.com>
2018-06-05 17:27:49 +0400

    #23 added general templates

--- icp-backend/listing-schedule/src/App.ts
+++ icp-backend/listing-schedule/src/App.ts
@@ -2,22 +2,13 @@ import * as path from 'path';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
-import * as passwordhash from 'password-hash'
-import * as passport from 'passport'
-import * as jwt from 'jsonwebtoken'
-import * as passportJWT from 'passport-jwt'
 var dotenv = require('dotenv').config({ path: path.join('.env') })
 var ibmdb = require('ibm_db');
 
 class App {
-  public jwtOptions: any = {};
-  public ExtractJwt = passportJWT.ExtractJwt;
-  public JwtStrategy = passportJWT.ExtractJwt;
   public express: express.Application;
   public connectionString: String;
   constructor() {
-    this.jwtOptions.jwtFromRequest = this.ExtractJwt.fromAuthHeaderAsBearerToken();
-    this.jwtOptions.secretOrKey = process.env.SECRET;
     this.connectionString = 'DATABASE=' + (process.env.DATABASE) + ';' +
       'HOSTNAME=' + process.env.HOSTNAME + ';' + 'UID=' + process.env.UID + ';' +
       'PWD=' + process.env.PASSWORD + ';' + 'PORT=' + process.env.PORT + ';' +
@@ -30,14 +21,13 @@ class App {
   private middleware(): void {
     this.express.use(logger('dev'));
     this.express.use(bodyParser.json());
-    this.express.use(passport.initialize());
     this.express.use(bodyParser.urlencoded({ extended: false }));
   }
   private routes(): void {
     let router = express.Router();
-    router.post('/login', (req, res, next) => {
+    router.post('/listFlights', (req, res, next) => {
       ibmdb.open(this.connectionString, function (err, conn) {
-        //service logic here
+       
       });
     });
     this.express.use('/', router);
