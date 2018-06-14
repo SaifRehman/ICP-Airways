5c151a102e4afc340751c4bdd733652fa4fd44fe
saifrehman <s4saif.121@gmail.com>
2018-06-15 01:34:01 +0400

    #62 added shell commands

--- icp-backend/db2-microservice/run.sh
+++ icp-backend/db2-microservice/run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-echo "hello"
+echo "building database ...."
 su - db2inst1 -c 'db2start'
 su - db2inst1 -c 'db2 create database SAMPLE'
 su - db2inst1 -c 'db2 connect to SAMPLE'
+su - db2inst1 -c 'wget https://raw.githubusercontent.com/SaifRehman/ICP-Airways/master/dataset/flights.csv'
+su - db2inst1 -c 'chown db2inst1:db2inst1 /home/db2inst1/flights.csv'
+# su - db2inst1 -c 'db2 CREATE TABLE 'SAMPLE.FlightsData (ID int NOT NULL , Year varchar(255) NULL , Month varchar(255) NULL, DayofMonth varchar(255) NULL, DepTime varchar(255) NULL,  CRSDepTime varchar(255) NULL, ArrTime varchar(255) NULL, CRSArrTime varchar(255) NULL, FlightNum varchar(255) NULL, TailNum varchar(255) NULL, ActualElapsedTime varchar(255) NULL, CRSElapsedTime varchar(255) NULL, Airtime varchar(255) NULL, ArrDelay varchar(255) NULL, DepDelay varchar(255) NULL,   Origin varchar(255) NULL, Dest varchar(255) NULL, Distance varchar(255) NULL, PRIMARY KEY (ID))''
