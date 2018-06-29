7b962bdf2aa00cd9546556f52e8b90ea2708e14e
SaifRehman <s4saif.121@gmail.com>
2018-06-30 01:45:08 +0400

    #91 refactored

--- icp-frontend/src/app/provider/provider.ts
+++ icp-frontend/src/app/provider/provider.ts
@@ -8,7 +8,7 @@ public token:any=null;
 constructor() {
     this.apiUrl={
         login:"http://loginsvc.my-namespace.svc.cluster.local/login",
-        signup:"http://signupsvc.my-namespace.svc.cluster.local/createUser",
+        signup:"http://signupsvc.my-namespace.svc.cluster.local:3004/createUser",
         book:"http://bookingsvc.my-namespace.svc.cluster.local/book",
         listBookingByUser:"http://bookingsvc.my-namespace.svc.cluster.local/listBookingByUser",
         checkin:"http://checkinsvc.my-namespace.svc.cluster.local/checkin",
