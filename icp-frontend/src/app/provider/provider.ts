4222ec6a79bd8b14918d4672ba2ae83a06747bbd
SaifRehman <s4saif.121@gmail.com>
2018-06-30 07:08:10 +0400

    #91 working with all nodeports

--- icp-frontend/src/app/provider/provider.ts
+++ icp-frontend/src/app/provider/provider.ts
@@ -7,13 +7,13 @@ public userData:any=null;
 public token:any=null;
 constructor() {
     this.apiUrl={
-        login:"http://loginsvc.my-namespace.svc.cluster.local/login",
+        login:"http://192.168.99.100:30096/login",
         signup:"http://192.168.99.100:30095/createUser",
-        book:"http://bookingsvc.my-namespace.svc.cluster.local/book",
-        listBookingByUser:"http://bookingsvc.my-namespace.svc.cluster.local/listBookingByUser",
-        checkin:"http://checkinsvc.my-namespace.svc.cluster.local/checkin",
-        listFlightsByID:"http://listingsvc.my-namespace.svc.cluster.local/listFlightsByID",
-        listFlights:"http://listingsvc.my-namespace.svc.cluster.local/listFlights"
+        book:"http://192.168.99.100:30099/book",
+        listBookingByUser:"http://192.168.99.100:30099/listBookingByUser",
+        checkin:"http://192.168.99.100:30098/checkin",
+        listFlightsByID:"http://192.168.99.100:30097/listFlightsByID",
+        listFlights:"http://192.168.99.100:30097/listFlights"
     }
   }
 }
