38da1ac45a120ba21a1d9554bf2f30b7d2ab9620
SaifRehman <s4saif.121@gmail.com>
2018-07-01 23:38:51 +0400

    #105 all services confugired with dns

--- icp-frontend/src/app/provider/provider.ts
+++ icp-frontend/src/app/provider/provider.ts
@@ -7,13 +7,13 @@ public userData:any=null;
 public token:any=null;
 constructor() {
     this.apiUrl={
-        login:"http://192.168.99.100:30096/login",
-        signup:"http://192.168.99.100:30095/createUser",
-        book:"http://192.168.99.100:30099/book",
-        listBookingByUser:"http://192.168.99.100:30099/listBookingByUser",
-        checkin:"http://192.168.99.100:30098/checkin",
-        listFlightsByID:"http://192.168.99.100:30097/listFlightsByID",
-        listFlights:"http://192.168.99.100:30097/listFlights"
+        login:"http://loginsvc.svc.cluster.local/login",
+        signup:"http://signupsvc.svc.cluster.local/createUser",
+        book:"http://bookingsvc.svc.cluster.local/book",
+        listBookingByUser:"http://bookingsvc.svc.cluster.local/listBookingByUser",
+        checkin:"http://checkinsvc.svc.cluster.local/checkin",
+        listFlightsByID:"http://listingsvc.svc.cluster.local/listFlightsByID",
+        listFlights:"http://listingsvc.svc.cluster.local/listFlights"
     }
   }
 }
