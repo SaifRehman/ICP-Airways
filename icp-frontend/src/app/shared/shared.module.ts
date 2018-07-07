61f1f37f4ef8f56e2202b2155d7d89e6cb6c2862
SaifRehman <s4saif.121@gmail.com>
2018-07-07 20:47:18 +0400

    #114 implemented angular pipes of country

--- icp-frontend/src/app/shared/shared.module.ts
+++ icp-frontend/src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
+import { CountryRefactorPipe } from './pipes/country.pipe';
 import { TimeRefactorPipe } from './pipes/time.pipe';
 import { NgModule} from '@angular/core'
 @NgModule({
-    declarations: [ TimeRefactorPipe ],
-    exports: [ TimeRefactorPipe ]
+    declarations: [ TimeRefactorPipe, CountryRefactorPipe ],
+    exports: [ TimeRefactorPipe, CountryRefactorPipe ]
 })
 export class SharedModule{
     
