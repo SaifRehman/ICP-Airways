359b253f9a84d325a3120468148b54e7ff0b2e9b
SaifRehman <s4saif.121@gmail.com>
2018-07-08 11:05:18 +0400

    ui integeration

--- icp-frontend/src/app/shared/pipes/time.pipe.ts
+++ icp-frontend/src/app/shared/pipes/time.pipe.ts
@@ -7,6 +7,9 @@ export class TimeRefactorPipe implements PipeTransform {
      private character:any=':'
      private position:any=2
     transform(value: any, ...args: any[]) {
+        if (value.length !==4 ){
+            value = '0'+value
+        }
         return [value.slice(0, this.position), this.character, value.slice(this.position)].join('');
     }
 }
