33a8cf57ee1aa03df0b4ea1295e49c859b029b6a
Amaan Kulshreshtha <amaan.kulshreshtha@gmail.com>
2018-07-09 23:14:50 +0400

    #107 frontend-bug-fix

--- icp-frontend/src/app/app.routing.ts
+++ icp-frontend/src/app/app.routing.ts
@@ -1,32 +1,32 @@
-import { RouterModule, Routes, CanActivate } from "@angular/router";
-import { ModuleWithProviders } from "@angular/core/src/metadata/ng_module";
-import { HistoryComponent } from "./history/history.component";
-import { LoginComponent } from "./login/login.component";
-import { SignupComponent } from "./signup/signup.component";
-import { HomeComponent } from "./home/home.component";
-import { AuthGuardService as AuthGuard } from "./guards/login-guard.service";
-import { LandingComponent } from "./landing/landing.component";
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+import { HistoryComponent } from './history/history.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuardService as AuthGuard } from './guards/login-guard.service';
+import { LandingComponent } from './landing/landing.component';
 export const AppRoutes: Routes = [
   {
-    path: "history",
-    component: HistoryComponent,
-    canActivate: [AuthGuard]
+    path: 'history',
+    component: HistoryComponent
+    // canActivate: [AuthGuard]
   },
   {
-    path: "home",
+    path: 'home',
     component: HomeComponent
     // canActivate: [AuthGuard]
   },
   {
-    path: "login",
+    path: 'login',
     component: LoginComponent
   },
   {
-    path: "signup",
+    path: 'signup',
     component: SignupComponent
   },
   {
-    path: "",
+    path: '',
     component: LandingComponent
   }
 ];
--- icp-frontend/src/app/landing/landing.component.ts
+++ icp-frontend/src/app/landing/landing.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { translateTrigger, slideTrigger } from '../animations';
-import { LoginService } from '../login/login.component.service'
-import { SignupService } from '../signup/signup.component.service'
+import { LoginService } from '../login/login.component.service';
+import { SignupService } from '../signup/signup.component.service';
 
 import { Provider } from '../provider/provider';
 import { RouterModule, Routes, Router } from '@angular/router';
-import * as jwtDecode from 'jwt-decode'
+import * as jwtDecode from 'jwt-decode';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -13,9 +13,9 @@ import * as jwtDecode from 'jwt-decode'
   animations: [translateTrigger]
 })
 export class LandingComponent implements OnInit {
-  email:String;
-  password:String;
-  public read:any;
+  email: String;
+  password: String;
+  public read: any;
   public displayHeading = false;
   public isLeftVisible = true;
   emailSignup: String;
@@ -24,38 +24,55 @@ export class LandingComponent implements OnInit {
   location: String;
   age: Number;
   passwordSignup: String;
-  constructor(public loginService:LoginService,  public provider:Provider, public router: Router,public signupService: SignupService) {
+  constructor(
+    public loginService: LoginService,
+    public provider: Provider,
+    public router: Router,
+    public signupService: SignupService
+  ) {
     setTimeout(() => {
       this.displayHeading = true;
     }, 1000);
   }
   ngOnInit() {}
-  login(){
-    this.loginService.login(this.email,this.password).subscribe((data) => {
-      console.log('data', data);
-      this.read=data;
-      this.read = this.read['token']
-      this.provider.token = this.read;
-      localStorage.setItem('token',this.provider.token);
-      this.provider.userData=jwtDecode(this.read);
-      this.router.navigateByUrl('/history');
-  },
-  (error) => {
-      alert("Login not Succesfull")
-      this.provider.userData=null;
-  });
-
+  login() {
+    this.loginService.login(this.email, this.password).subscribe(
+      data => {
+        console.log('data', data);
+        this.read = data;
+        this.read = this.read['token'];
+        this.provider.token = this.read;
+        localStorage.setItem('token', this.provider.token);
+        this.provider.userData = jwtDecode(this.read);
+        this.router.navigateByUrl('/history');
+      },
+      error => {
+        alert('Login not Succesfull');
+        this.provider.userData = null;
+      }
+    );
   }
 
   signup() {
-    this.signupService.signup(this.firstName, this.lastName, this.age, this.email, this.password, this.location).subscribe((data) => {
-      console.log('data', data);
-      this.read = data;
-      alert('Signup sucessfull')
-    },
-      (error) => {
-        console.log(error)
-        alert('signup not sucessfull')
-      });
+    this.signupService
+      .signup(
+        this.firstName,
+        this.lastName,
+        this.age,
+        this.email,
+        this.password,
+        this.location
+      )
+      .subscribe(
+        data => {
+          console.log('data', data);
+          this.read = data;
+          alert('Signup sucessfull');
+        },
+        error => {
+          console.log(error);
+          alert('signup not sucessfull');
+        }
+      );
   }
 }
