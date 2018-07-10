e20ac14de96d4702760dde54d3fba713fca02170
SaifRehman <s4saif.121@gmail.com>
2018-07-10 23:04:59 +0400

    auth guard refactored

--- icp-frontend/src/app/guards/login-guard.service.ts
+++ icp-frontend/src/app/guards/login-guard.service.ts
@@ -6,7 +6,7 @@ export class AuthGuardService implements CanActivate {
   constructor(public auth: AuthService, public router: Router) {}
   canActivate(): boolean {
     if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['login']);
+      this.router.navigate(['']);
       return false;
     }
     return true;
