578034a26c85796397276418778e5eb267509711
SaifRehman <s4saif.121@gmail.com>
2018-07-11 19:32:20 +0400

    #121 refactored all for ethereum deployment

--- IcpContract/contracts/IcpContract.sol
+++ IcpContract/contracts/IcpContract.sol
@@ -1,12 +1,13 @@
 //Write your own contracts here. Currently compiles using solc v0.4.15+commit.bbb8e64f.
 pragma solidity ^0.4.18;
-contract JsonStore {
+contract IcpContract {
+
     function set(string _value) public {
         value = _value;
     }
+    
     function get() public constant returns (string) {
         return value;
     }
-
     string value;
 }
--- IcpContract/truffle.js
+++ IcpContract/truffle.js
@@ -15,9 +15,16 @@
 module.exports = {
   networks: {
     development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    external: {
       host: "169.61.62.89",
       port: 30001,
-      network_id: "*" // Match any network id
+      from: "0xd5335aee753f741c35a4ea5eb59dd4937827d8a9",
+      network_id: "1",
+      gas: 500000,
     }
   }
 };
