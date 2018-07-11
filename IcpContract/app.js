aebcd51a8557150c77dab5609a12b065c1d36dd1
SaifRehman <s4saif.121@gmail.com>
2018-07-12 02:12:43 +0400

    121 ethereum microservice complete

--- IcpContract/app.js
+++ IcpContract/app.js
@@ -52,47 +52,13 @@ let provider = new Web3.providers.HttpProvider("http://169.61.62.89:30001");
 const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(abi,"0x03a8cad9c7a5969b7382459c6f5fb2808fe3f178");
 
-
-// var callData=contract.methods.get()
-// console.log(callData)
-contract.methods.set('saif').call().then(function(res){
-    console.log('aaaaa',res);
-}).catch(function(err) {
-    console.log(err);
+contract.methods.set("[]").send({
+    from: '0xd5335aee753f741c35a4ea5eb59dd4937827d8a9',
+    gas: '1000000'
 });
+
 // contract.methods.get().call().then(function(res){
 //     console.log(res);
 // }).catch(function(err) {
 //     console.log(err);
 // });
-// var Web3 = require('web3'); //need web3 1.0
-// var fs = require('fs'); //need web3 1.0
-
-// var web3 = new Web3(new Web3.providers.HttpProvider('http://169.61.62.89:30001'));
-// var accounts;
-// web3.eth.getAccounts().then((acc) => accounts = acc);
-
-// //Compile contracts with solc, the solidity compiler
-// var solc = require('solc');
-// var contractSrc = fs.readFileSync('./contracts/IcpContract.sol').toString();
-// var contractCom = solc.compile(contractSrc);
-
-// //Create contract object
-// var abi = JSON.parse(contractCom.contracts[':IcpContract'].interface);
-// var HelloWorld = new web3.eth.Contract(abi);
-
-// //Build transaction object;
-// var bytecode = contractCom.contracts[':IcpContract'].bytecode;
-// var argHex = web3.utils.asciiToHex("hey"); //Needed because our contract constructor needs a bytes32 type
-// var deployContractTx = HelloWorld.deploy({data: bytecode, arguments: [argHex]});
-
-
-
-// var callData=democontract.returnValue.call();
-// console.log(callData); 
-// //Deploy to the network
-// // var contractInstance;
-// // deployContractTx.send({from: '0xd5335aee753f741c35a4ea5eb59dd4937827d8a9', gas: 1000000}).then((instance) => contractInstance = instance);
-
-// // //Call a method
-// // contractInstance.methods.get().call().then((result) => console.log(web3.utils.hexToAscii(result)));
---
+++ icp-backend/ethereum-microservice/src/App.ts
@@ -0,0 +1,120 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as passwordhash from 'password-hash'
+import * as passport from 'passport'
+import * as jwt from 'jsonwebtoken'
+import * as passportJWT from 'passport-jwt'
+var Web3 = require('web3')
+
+class App {
+  public jwtOptions: any = {};
+  public ExtractJwt = passportJWT.ExtractJwt;
+  public JwtStrategy = passportJWT.ExtractJwt;
+  public express: express.Application;
+  public connectionString: String;
+  public abi: any = null;
+  public provider: any;
+  public web3: any;
+  public contract: any
+  constructor() {
+    this.jwtOptions.jwtFromRequest = this.ExtractJwt.fromAuthHeaderAsBearerToken();
+    this.jwtOptions.secretOrKey = process.env.SECRET;
+    this.abi = [
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "value",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_value",
+            "type": "string"
+          }
+        ],
+        "name": "set",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "get",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]
+    this.provider = new Web3.providers.HttpProvider("http://169.61.62.89:30001");
+    this.web3 = new Web3(this.provider);
+    this.contract = new this.web3.eth.Contract(this.abi, "0x03a8cad9c7a5969b7382459c6f5fb2808fe3f178");
+    this.express = express();
+    this.middleware();
+    this.routes();
+  }
+  private ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[1];
+      req.token = bearerToken;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
+  private middleware(): void {
+    this.express.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "*");
+      next();
+    });
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(passport.initialize());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+  }
+  private routes(): void {
+    let router = express.Router();
+    router.post('/setBlockchain', this.ensureToken, (req, res, next) => {
+      this.contract.methods.set(req.body.data).send({
+        from: '0xd5335aee753f741c35a4ea5eb59dd4937827d8a9',
+        gas: '1000000'
+      });
+      res.json({
+        message: "sucessful"
+      });
+    });
+    router.get('/getBlockchain', this.ensureToken, (req, res, next) => {
+      this.contract.methods.get().call().then(function (res) {
+        res.json({
+          data: JSON.parse(res)
+        });
+      }).catch(function (err) {
+        console.log(err);
+      });
+    });
+    this.express.use('/', router);
+  }
+}
+export default new App().express;
