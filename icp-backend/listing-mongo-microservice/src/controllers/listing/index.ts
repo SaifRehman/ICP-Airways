8e0546f6d2399fbc87f5439911030e4007aa5bd2
Saif Ur Rehman <s4saif.121@gmail.com>
2018-11-19 16:21:28 +0400

    #175 comleted flightcontroller

--- icp-backend/listing-mongo-microservice/src/controllers/listing/index.ts
+++ icp-backend/listing-mongo-microservice/src/controllers/listing/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { Request, Response } from "express";
+import { FlightController } from '../listing/lib/controllers/crmController';
+
 import "reflect-metadata";
 class ListingMongoController {
+  public flightController: FlightController = new FlightController() 
   public router: Router;
 
   constructor() {
@@ -14,9 +17,9 @@ class ListingMongoController {
       res.status(200).send("healthz");
     });
 
-    this.router.get("/listFlights", (_, res: Response) => {
+    this.router.get("/listFlights", (req: Request, res: Response) => {
       res.send("listFlights get");
-    });
+    },this.flightController.getFlights);
 
     this.router.post("/listFlights", (req: Request, res: Response) => {
       res.send("listFlights post");
