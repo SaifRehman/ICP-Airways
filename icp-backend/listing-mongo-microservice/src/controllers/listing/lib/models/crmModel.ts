0544ff9c534fa0e09aaf6417c5899f143603c3d5
Saif Ur Rehman <s4saif.121@gmail.com>
2018-11-19 16:06:21 +0400

    #175 comleted flightcontroller

--- icp-backend/listing-mongo-microservice/src/controllers/listing/lib/models/crmModel.ts
+++ icp-backend/listing-mongo-microservice/src/controllers/listing/lib/models/crmModel.ts
@@ -3,6 +3,9 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const FlightSchema = new Schema({
+    ID: {
+        type: Number
+    },
     Year: {
         type: String
     },
