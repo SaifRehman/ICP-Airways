d0a95b027d8b2cdd7995903f65fc216c66723e42
Saif Ur Rehman <s4saif.121@gmail.com>
2018-11-20 10:13:46 +0400

    #175 refactored error handling

--- icp-backend/listing-mongo-microservice/src/controllers/listing/lib/controllers/crmController.ts
+++ icp-backend/listing-mongo-microservice/src/controllers/listing/lib/controllers/crmController.ts
@@ -5,48 +5,56 @@ import { Request, Response } from "express";
 const Flight = mongoose.model("Flight", FlightSchema);
 export class FlightController {
   public addNewFlight(req: Request, res: Response) {
+    res.setHeader('Content-Type', 'application/json');
     let newFlight = new Flight(req.body);
     newFlight.save((err, flight) => {
       if (err) {
-        res.status(400).send(err);
-      }
+        res.status(404).json({ err });
+        return;
+    }
       res.json(flight);
     });
   }
   public getFlights(req: Request, res: Response) {
+    res.setHeader('Content-Type', 'application/json');
     Flight.find({}, (err, flight) => {
       if (err) {
-        res.status(400).send(err);
-      } else {
+        res.status(404).json({ err });
+        return;
+    } else {
         res.status(200).send(flight);
       }
     });
   }
   public getFlightById(req: Request, res: Response) {
+    res.setHeader('Content-Type', 'application/json');
     Flight.findById(req.params.ID, (err, flight) => {
-        console.log(req.params.ID)
       if (err) {
-        res.status(400).send(err);
-      } else {
+        res.status(404).json({ err });
+        return;      } else {
         res.status(200).send(flight);
       }
     });
   }
   public updateFlight(req: Request, res: Response) {
+    res.setHeader('Content-Type', 'application/json');
+
     Flight.findOneAndUpdate(
       { _id: req.params.ID }, req.body, { new: true },(err, flight) => {
         if (err) {
-          res.status(400).send(err);
-        }
+            res.status(404).json({ err });
+            return;        }
         res.json(flight);
       }
     );
   }
 
-  public deleteFlight (req: Request, res: Response) {           
+  public deleteFlight (req: Request, res: Response) {   
+    res.setHeader('Content-Type', 'application/json');        
     Flight.deleteOne({ _id: req.params.ID }, (err) => {
         if(err){
-            res.status(400).send(err);
+            res.status(404).json({ err });
+            return;
         }
         res.json({ message: "success"});
     });
