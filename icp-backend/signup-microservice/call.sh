6d5ad941e9a43bd8f3589552ae505608b9836ba8
Saif Ur Rehman <s4saif.121@gmail.com>
2018-12-05 18:07:34 +0400

    refactored helm charts

--- icp-backend/signup-microservice/call.sh
+++ icp-backend/signup-microservice/call.sh
@@ -1,3 +1,3 @@
-time for i in {1..100}; do
-    seq 10000 | parallel -n0 -j1000  "curl http://158.175.115.86:30409/healthz -m 10000"
+time for i in {1..1000}; do
+    seq 100 | parallel -n0 -j100  "curl https://10.150.20.151/listingsvc/listFlights --insecure -m 100"
   done
