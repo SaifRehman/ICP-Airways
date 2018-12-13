f9ac4443038a9bce7ff40474bb92125a57a6032b
Saif Ur Rehman <s4saif.121@gmail.com>
2018-12-13 17:19:01 +0400

    added circuitbreaker gateway poolejection retry and timeout policies

--- icp-backend/listing-mongo-microservice/tx.sh
+++ icp-backend/listing-mongo-microservice/tx.sh
@@ -1,3 +1,3 @@
 time for i in {1..100}; do
-    seq 1000 | parallel -n0 -j5  "curl -H 'Content-Type: application/json' https://10.150.20.151/listingsvc/healthz --insecure -m 1000"
+    seq 1000 | parallel -n0 -j5  "curl -H 'Content-Type: application/json' http://10.150.20.151:31380/listingsvc/healthz --insecure -m 1000"
   done
