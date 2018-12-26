c17b830b491e0345f433a92138a43f6215acdf1c
Saif Ur Rehman <s4saif.121@gmail.com>
2018-12-26 06:55:04 +0400

    added ionic pipes

--- icp-mobile/src/pages/map/map.ts
+++ icp-mobile/src/pages/map/map.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import leaflet from "leaflet";
+import 'leaflet-routing-machine';
 
 @IonicPage()
 @Component({
