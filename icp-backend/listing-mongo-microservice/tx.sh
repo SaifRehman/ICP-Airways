65f4d491edb2aba1842c74175d10994c46524193
Saif Ur Rehman <s4saif.121@gmail.com>
2019-01-03 12:26:54 +0400

    added cron job

--- icp-backend/listing-mongo-microservice/tx.sh
+++ icp-backend/listing-mongo-microservice/tx.sh
@@ -1,3 +1,3 @@
-time for i in {1..100}; do
-    seq 1000 | parallel -n0 -j5  "curl -H 'Content-Type: application/json' http://10.150.20.151:31380/listingsvc/healthz --insecure -m 1000"
+time for i in {1..2000}; do
+    seq 1000 | parallel -n0 -j5  "curl -H 'Content-Type: application/json' https://google.com --insecure -m 1000"
   done
